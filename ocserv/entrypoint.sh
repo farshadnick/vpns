da645749a65e082d134d655e342900ab8b477b84
Farshad Nick <88557305+farshadnick@users.noreply.github.com>
2022-11-18 23:44:46 +0330

    Create entrypoint.sh

---
+++ ocserv/entrypoint.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+set -e
+iptables -t nat -A POSTROUTING -j MASQUERADE
+echo "net.ipv4.ip_forward = 1" >> /etc/sysctl.conf
+sysctl -p
+
+
+                CA_CN="VPN CA"
+                CA_ORG="Big Corp"
+                CA_DAYS=9999
+                SRV_CN="www.example.com"
+                SRV_ORG="MyCompany"
+                SRV_DAYS=9999
+
+        # No certification found, generate one
+        cd /etc/ocserv/certs
+        certtool --generate-privkey --outfile ca-key.pem
+        cat > ca.tmpl <<-EOCA
+        cn = "$CA_CN"
+        organization = "$CA_ORG"
+        serial = 1
+        expiration_days = $CA_DAYS
+        ca
+        signing_key
+        cert_signing_key
+        crl_signing_key
+        EOCA
+        certtool --generate-self-signed --load-privkey ca-key.pem --template ca.tmpl --outfile ca.pem
+        certtool --generate-privkey --outfile server-key.pem
+        cat > server.tmpl <<-EOSRV
+        cn = "$SRV_CN"
+        organization = "$SRV_ORG"
+        expiration_days = $SRV_DAYS
+        signing_key
+        encryption_key
+        tls_www_server
+        EOSRV
+        certtool --generate-certificate --load-privkey server-key.pem --load-ca-certificate ca.pem --load-ca-privkey ca-key.pem --template server.tmpl --outfile server-cert.pem
+
+exec "$@"
